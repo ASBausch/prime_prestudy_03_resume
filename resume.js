64904e36727074fd7d7fd31ade0c2877963c187d
Amanda Bausch <manadab@me.com>
2015-11-06 11:05:24 -0600

    added jquery methods to vin diagram content

--- resume.js
+++ resume.js
@@ -4,6 +4,40 @@ $(document).ready(function(){
 		$('button').hide();
 		$('main').fadeIn(1000);
 	});	
+
+//adds explanation years to vin circle labels and bolds upon click
+//needs refactoring
+//can I refactor these functions to make ONE
+	
+	$('p.labelyellow').one('click', function(){
+		var years = '<br />8years'
+		$(this).append(years);
+		$(this).addClass('labelchange');
+	});
+
+	$('p.labelorange').one('click', function(){
+		var years = '<br />6years'
+		$(this).append(years);
+		$(this).addClass('labelchange');
+
+	});
+
+	//why did "this" work in the selectors but not the full attribute list?
+
+	$('p.labelgreen').one('click', function(){
+		var years = '<br />14years'
+		$(this).append(years);
+		$(this).addClass('labelchange');
+
+	});
+
+	$('p.labelblue').one('click', function(){
+		var years = '<br />20years'
+		$(this).append(years);
+		$(this).addClass('labelchange');
+
+	});
+
 	
 });
 
